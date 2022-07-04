112915e70fd69727a1c450d0b94e985106bff07e
Marrymangal <78206403+Marrymangal@users.noreply.github.com>
2022-07-04 18:28:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+var obstacleTop, obsTop1, obsTop2
+var obstacleBottom, obsBottom1, obsBottom2, obsBottom3
+var gameOver, gameOverImg
+var restart, restartImg
+
+var score = 0;
+
+//game states      
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+function preload(){
+
+
+}
+
+function setup(){
+
+ 
+}
+
+function draw() {
+  
+  background("black");
+
+  
+
+}
+
+
+function spawnObstaclesTop() 
+{
+  
+
+}
+
+function spawnObstaclesBottom() 
+{
+      
+}
+
+ function Bar() 
+ {
+         
+}
+
+function Score()
+{
+       
+  
+}
+
+  
